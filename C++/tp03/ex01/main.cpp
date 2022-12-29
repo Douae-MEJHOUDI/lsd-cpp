85e2754c2b731c569a3d277626b554e60e0a06cc
Douae-MEJHOUDI <92058213+Douae-MEJHOUDI@users.noreply.github.com>
2022-12-29 05:03:01 +0100

    main.cpp ex01

---
+++ C++/tp03/ex01/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include "class.hpp"
+#include "class.cpp"
+
+using namespace std;
+
+int main()
+{
+	string end= "ok";
+	Phonebook P;
+	cout<<"Hi dear user"<<endl;
+	do
+	{
+		int action;
+		cout<<"Please print\n 1 -> To add new contact to the phonebook \n 2 -> To search a contact \n 3 -> To clear the phonebook "<<endl;
+		cin>>action;
+		switch (action)
+		{
+			case 1: 
+				P.ADD();
+				break;
+			case 2:
+				P.search();
+				break;
+			case 3:
+				P.exit();
+				exit(EXIT_SUCCESS);
+				break;
+			default:
+				cout<<"This is an invalid option "<<endl;
+				break;
+		}
+		cout<<"if u wanna quit print EXIT, otherwise print anything else: ";
+		cin>>end;
+	}while(end != "EXIT");
+	return 0;
+}
